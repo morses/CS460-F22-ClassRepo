0a816d54e4d071b16db50ec629f31424733cfa9c
Scot Morse <morses@wou.edu>
2022-10-19 15:44:07 -0700

    add version of AuctionHouse example to the lecture examples that shows CRUD operations using Repositories, including generic versions of Repositories

---
+++ lecture_examples/AuctionHouseWithGenericRepository/AuctionHouse/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AuctionHouse.Models;
+using AuctionHouse.DAL.Abstract;
+
+namespace AuctionHouse.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private IBuyerRepository _buyerRepo;
+
+    public HomeController(ILogger<HomeController> logger, IBuyerRepository buyerRepo)
+    {
+        _logger = logger;
+        //_context = ctx;
+        _buyerRepo = buyerRepo;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Buyers()
+    {
+        //IEnumerable<Buyer> buyers = _context.Buyers.ToList();
+        int count = _buyerRepo.NumberOfBuyers();
+        IEnumerable<Buyer> buyers = _buyerRepo.GetAll();        // calling base class methods
+        List<string> emailList = _buyerRepo.EmailList();        // calling derived class methods
+        return View(buyers);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ lecture_examples/AuctionHouseWithGenericRepository/AuctionHouse/Program.cs
@@ -0,0 +1,55 @@
+namespace AuctionHouse;
+using AuctionHouse.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AuctionHouse.DAL.Abstract;
+using AuctionHouse.DAL.Concrete;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<AuctionHouseDbContext>(
+            options => options
+                        .UseLazyLoadingProxies()    // Will use lazy loading, but not in LINQPad as it doesn't run Program.cs
+                        .UseSqlServer(builder.Configuration.GetConnectionString("AuctionHouseConnection"))
+                        
+        );
+        builder.Services.AddScoped<DbContext, AuctionHouseDbContext>();             // Need this line since our generic repository is based on DbContext directly
+        builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));    // Easy way to register all the generic repositories 
+        builder.Services.AddScoped<IBuyerRepository, BuyerRepository>();  // Register our custom types (if needed) with the dependency injection container
+        //builder.Services.AddScoped<IBidRepository, BidRepository>();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        else
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
