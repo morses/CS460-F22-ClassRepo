b5651c6f6e747fa4bb3226c29ff8aba65bf44c6a
Scot Morse <morses@wou.edu>
2022-10-20 14:01:10 -0700

    add scaffolded CRUD for Items in the AuctionHouse example

---
+++ lecture_examples/AuctionHouse/AuctionHouse/Controllers/ItemController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AuctionHouse.Models;
+
+namespace AuctionHouse.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly AuctionHouseDbContext _context;
+
+        public ItemController(AuctionHouseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Item
+        public async Task<IActionResult> Index()
+        {
+            var auctionHouseDbContext = _context.Items.Include(i => i.Seller);
+            return View(await auctionHouseDbContext.ToListAsync());
+        }
+
+        // GET: Item/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Seller)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: Item/Create
+        public IActionResult Create()
+        {
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Item/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,Condition,SellerId")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "Id", "Id", item.SellerId);
+            return View(item);
+        }
+
+        // GET: Item/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "Id", "Id", item.SellerId);
+            return View(item);
+        }
+
+        // POST: Item/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Condition,SellerId")] Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SellerId"] = new SelectList(_context.Sellers, "Id", "Id", item.SellerId);
+            return View(item);
+        }
+
+        // GET: Item/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Items == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Seller)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Items == null)
+            {
+                return Problem("Entity set 'AuctionHouseDbContext.Items'  is null.");
+            }
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(int id)
+        {
+          return (_context.Items?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
