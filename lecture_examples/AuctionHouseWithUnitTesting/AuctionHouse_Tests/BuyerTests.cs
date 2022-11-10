e5eb3a6b24d1e0308bff8e97359ccf490e7e967c
Scot Morse <morses@wou.edu>
2022-11-10 09:22:11 -0800

    add unit tests of model validation

---
+++ lecture_examples/AuctionHouseWithUnitTesting/AuctionHouse_Tests/BuyerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AuctionHouse.Models;
+
+namespace AuctionHouse_Tests
+{
+    public class BuyerTests
+    {
+        private Buyer MakeValidBuyer()
+        {
+            Buyer buyer = new Buyer
+            {
+                Id = 1,
+                FirstName = "Jimmie",
+                LastName = "Peters",
+                Email = "jpeters@gmail.com",
+                Bids = new List<Bid>()
+            };
+            return buyer;
+        }
+
+        [Test]
+        public void ValidBuyer_IsValid()
+        {
+            // Arrange
+            Buyer buyer = MakeValidBuyer();
+
+            // Act
+            ModelValidator mv = new ModelValidator(buyer);
+
+            // Assert
+            Assert.That(mv.Valid, Is.True);
+        }
+
+        [Test]
+        public void Buyer_WithMissingFirstName_IsNotValid()
+        {
+            // Arrange
+            Buyer buyer = MakeValidBuyer();
+            buyer.FirstName = null!;
+
+            // Act
+            ModelValidator mv = new ModelValidator(buyer);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(mv.Valid, Is.False);
+                Assert.That(mv.ContainsFailureFor("FirstName"), Is.True);
+            });
+        }
+
+        [Test]
+        public void Buyer_WithEmptyStringForFirstName_IsNotValid()
+        {
+            // Arrange
+            Buyer buyer = MakeValidBuyer();
+            buyer.FirstName = "";
+
+            // Act
+            ModelValidator mv = new ModelValidator(buyer);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(mv.Valid, Is.False);
+                Assert.That(mv.ContainsFailureFor("FirstName"), Is.True);
+            });
+        }
+
+        [Test]
+        public void Buyer_WithTooLongFirstName_IsNotValid()
+        {
+            // Arrange
+            Buyer buyer = MakeValidBuyer();
+            buyer.FirstName = "PhilipJohannaDeweyKarlCoryConnieBrendanLolaJimmieVivian";
+
+            // Act
+            ModelValidator mv = new ModelValidator(buyer);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(mv.Valid, Is.False);
+                Assert.That(mv.ContainsFailureFor("FirstName"), Is.True);
+            });
+        }
+
+        // Continue on in this fashion, both positive and negative cases
+    }
+}
