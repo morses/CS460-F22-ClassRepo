869e22f84bdda24f21ca5b1b819b523f8370208d
Scot Morse <morses@wou.edu>
2022-11-10 08:49:30 -0800

    add new AuctionHouse project to show unit testing with mocking; same as the AuctionHouseWithGenericRepository but have added an Nunit testing project with reference to the main project

---
+++ lecture_examples/AuctionHouseWithUnitTesting/AuctionHouse/Models/AuctionHouseDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AuctionHouse.Models
+{
+    public partial class AuctionHouseDbContext : DbContext
+    {
+        public AuctionHouseDbContext()
+        {
+        }
+
+        public AuctionHouseDbContext(DbContextOptions<AuctionHouseDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Bid> Bids { get; set; } = null!;
+        public virtual DbSet<Buyer> Buyers { get; set; } = null!;
+        public virtual DbSet<Item> Items { get; set; } = null!;
+        public virtual DbSet<Seller> Sellers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=AuctionHouseConnection");
+            }
+            optionsBuilder.UseLazyLoadingProxies();
+            // now works for LINQPad
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bid>(entity =>
+            {
+                entity.HasOne(d => d.Buyer)
+                    .WithMany(p => p.Bids)
+                    .HasForeignKey(d => d.BuyerId)
+                    .HasConstraintName("Bid_Fk_Buyer");
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.Bids)
+                    .HasForeignKey(d => d.ItemId)
+                    .HasConstraintName("Bid_Fk_Item");
+            });
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.HasOne(d => d.Seller)
+                    .WithMany(p => p.Items)
+                    .HasForeignKey(d => d.SellerId)
+                    .HasConstraintName("Item_Fk_Seller");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ lecture_examples/AuctionHouseWithUnitTesting/AuctionHouse/Models/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionHouse.Models
+{
+    [Table("Item")]
+    public partial class Item
+    {
+        public Item()
+        {
+            Bids = new HashSet<Bid>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+        [StringLength(256)]
+        public string Description { get; set; } = null!;
+        [StringLength(512)]
+        public string Condition { get; set; } = null!;
+        [Column("SellerID")]
+        public int? SellerId { get; set; }
+
+        [ForeignKey("SellerId")]
+        [InverseProperty("Items")]
+        public virtual Seller? Seller { get; set; }
+        [InverseProperty("Item")]
+        public virtual ICollection<Bid> Bids { get; set; }
+    }
+}
