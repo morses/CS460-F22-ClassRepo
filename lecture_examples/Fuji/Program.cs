3f86dc1bbb470aabe2b8edbf5b67e4307033b72a
Scot Morse <morses@wou.edu>
2023-03-14 15:04:42 -0700

    adding a minimal example of seeding users and adding an admin role; is not the full Fuji example from the videos

---
+++ lecture_examples/Fuji/Program.cs
@@ -0,0 +1,110 @@
+using Fuji.Data;
+using Fuji.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace Fuji
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("AuthenticationConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(connectionString));
+            builder.Services.AddDbContext<FujiDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("FujiConnection")));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            // Customize some settings that Identity uses
+            builder.Services.Configure<IdentityOptions>(opts => {
+                // lines without comments are the default settings that we get by "AddDefault" above
+                opts.Password.RequireDigit = true;
+                opts.Password.RequireLowercase = true;
+                opts.Password.RequireNonAlphanumeric = false;   // default is true
+                opts.Password.RequireUppercase = true;
+                opts.Password.RequiredLength = 8;           // default is 6
+                opts.Password.RequiredUniqueChars = 5;      // default is 1
+                opts.SignIn.RequireConfirmedEmail = false;
+                opts.SignIn.RequireConfirmedPhoneNumber = false;
+                opts.User.AllowedUserNameCharacters =
+            "abcdefghijklmnopqrstuvwxyzBCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";   // removed letter 'A'
+                opts.User.RequireUniqueEmail = true;        // default is false
+            });
+            builder.Services.AddControllersWithViews();
+
+            // Change the overriding authorization strategy from 
+            //     "allow unless explicitly authorized" (the default)
+            // to
+            //     "require authorization unless explicitly allowed for anyone"
+            builder.Services.AddAuthorization(opts => {
+                opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+                            .RequireAuthenticatedUser()
+                            .Build();
+            });
+
+            var app = builder.Build();
+
+            // By using a scope for the services to be requested below, we limit their lifetime to this set of calls.
+            // See: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-5.0#call-services-from-main
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    // Get the IConfiguration service that allows us to query user-secrets and 
+                    // the configuration on Azure
+                    var config = app.Services.GetRequiredService<IConfiguration>();
+                    // Set password with the Secret Manager tool, or store in Azure app configuration
+                    // dotnet user-secrets set SeedUserPW <pw>
+
+                    var testUserPw = config["SeedUserPW"];
+                    var adminPw = config["SeedAdminPW"];
+
+                    SeedUsers.Initialize(services, SeedData.UserSeedData, testUserPw).Wait();
+                    SeedUsers.InitializeAdmin(services, "admin@example.com", "admin", adminPw, "The", "Admin").Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
---
+++ lecture_examples/Fuji/README.txt
@@ -0,0 +1,2 @@
+﻿
+This project isn't the complete Fuji example.  It is just a minimal example of seeding users including the Admin, with the admin role.
